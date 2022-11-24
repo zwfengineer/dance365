a822d4b848ce83582d0e466039982a016f25e421
李焕琴 <11394114+lihuanqin@user.noreply.gitee.com>
2022-11-25 00:56:15 +0800

    ...

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -1,12 +1,29 @@
 import { RouteRecordRaw } from "vue-router";
 
-export const  staticRouters:Array<RouteRecordRaw>=[
-    {
-        path:"/home",
-        name:"home",
-        component:()=>import("@/views/home/index.vue")
-    },{
-        path:"/",
-        redirect:"/home"
-    }
-]
+export const staticRouters: Array<RouteRecordRaw> = [
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/home/index.vue"),
+  },
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("@/views/error/404.vue"),
+    meta: {
+      hidden: true,
+    },
+  },
+  {
+    path: "/:pathMatch(.*)",
+    name: "Any",
+    redirect: "/404",
+    meta: {
+      hidden: true,
+    },
+  },
+];
