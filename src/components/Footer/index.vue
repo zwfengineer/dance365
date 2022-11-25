3ba5ce1873c8502437c4ce4f1f710b0da5ddc39f
hhy <1654620778@qq.com>
2022-11-25 14:51:47 +0800

    测试能否上传

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    footer
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name:'Footer',
+})
+
+</script>
+
+<style scoped>
+
+</style>
